34ddf8e17a2f799016e810534d79a80578078da0
Mehedi-68 <ormehediop@gmail.com>
2024-10-04 07:50:24 +0600

    3rd

--- dateandtime.py
+++ dateandtime.py
@@ -6,3 +6,10 @@ print(currunt_dt)
 
 formeting_datetime = currunt_dt.strftime("%d-%m-%y  %H:%M:%S ")
 print(formeting_datetime)
+
+
+date1 = datetime.timedelta(2024,1,4)
+date2 = datetime.timedelta(2023,4,6)
+
+defarence = date1 - date2
+print(defarence)

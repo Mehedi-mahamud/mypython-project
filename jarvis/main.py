bdc15304b46313f370cd698ea3da0afa7aa34361
Mehedi-68 <ormehediop@gmail.com>
2024-10-12 14:38:39 +0600

    ading jarvis

---
+++ jarvis/main.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(test):
+    engine.say(test)
+    engine.runAndWait()
+
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    else:
+        pass
+        # Let OpenAI handle the request
+        # output = aiProcess(c)
+        # speak(output) 
+
+
+if __name__ == "__main__":
+    speak("Initilizing Jarvis")
+
+    while True:
+       
+        r = sr.Recognizer()
+         
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+        except Exception as e:
+            print("Error; {0}".format(e))
